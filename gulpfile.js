3a2125c67d273269e08bb44dcc01ead2d76d5e17
Caree Claunch <caree@caree.codes>
2017-10-14 22:00:21 -0500

    typescript compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 // Requirements
+// ==================================================================
 var gulp = require('gulp');
 
 // https://www.npmjs.com/package/gulp-sass
 var sass = require('gulp-sass');
 
+// https://www.npmjs.com/package/gulp-typescript
+var ts = require('gulp-typescript');
+
+
+
 // Default and basic tasks
+// ==================================================================
+
 gulp.task('default', function(){
     // TODO: automatically loop through all gulp.tasks and output them to the log when gulp is called by the user
     console.log('Welcome to Gulp. Available commands: test, hey');
