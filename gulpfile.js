9560d5bee40ac3dd9d1cbd571ef508611b3a9c0c
Caree Claunch <caree@caree.codes>
2017-10-14 21:26:33 -0500

    made my first functioning gulpfile...sort of.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
-// Hello World!
+// ==========================================================================
+// Welcome to Caree's messy, experimental dive into writing Gulp files. 
+// This is a work progress. Watch where you step.
+// ==========================================================================
+
+
+// Gotta require gulp if you wanna gulp
+var gulp = require('gulp');
+
+// Default task so that the 'gulp' command actually does something.
+gulp.task('default', function(){
+    console.log('ayyyy');
+});
