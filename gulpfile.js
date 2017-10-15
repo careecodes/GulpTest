3b61d3fe05c52197415728996eb67a00119c8cfa
Caree Claunch <caree@caree.codes>
2017-10-14 21:29:40 -0500

    added some extra functionality???

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,13 @@ var gulp = require('gulp');
 
 // Default task so that the 'gulp' command actually does something.
 gulp.task('default', function(){
-    console.log('ayyyy');
+    console.log('Welcome to Gulp. Available commands: test, hey');
 });
 
 gulp.task('test', function(){
     console.log('Gulp is working');
+});
+
+gulp.task('hey', function(){
+    console.log('ayy');
 });
